3f656be3177c87529429230c71faa3940f991bb8
Valentin Venzhega <valek917@yandex.ru>
2021-02-04 20:25:22 +0300

    дз к 6 уроку

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let isNumber = function(n){
+   return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+function foo() {
+   const a = +prompt('Угадай число от 1 до 100'),
+      b = 20;
+   console.log(a);
+   if( a > 100 && a !== b) {
+      alert('Загаданное число меньше');
+      foo();
+   } else if( a < 1 && a !== b && a !== 0) {
+      alert('Загаданное число больше');
+      foo();
+   }  else if ( 1 <= a <= 100 && a === b) {
+      alert('Поздравляю, Вы угадали!!!');
+   } else if( a >= 1 && a <= 100 && a !== b) {
+      alert('Угадай число от 1 до 100');
+      foo();
+   }else if( !isNumber(a)) {
+      alert('Введи число!');
+      foo();
+   } else if (a === 0) {
+      alert('Игра окончена');
+   } else {
+      foo();
+   }
+}
+foo();
+
